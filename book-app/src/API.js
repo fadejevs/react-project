a4cb160b1533e768586ac96d18436a78ceb975e5
Ralfs Fadejevs <ralfsfadejevs@ralfss-macbook-pro.home>
2022-11-15 14:10:30 +0100

    finalised,needed routes have been set and API's have been double checked for functionality.

--- book-app/src/API.js
+++ book-app/src/API.js
@@ -1,3 +1,2 @@
 export const API_URL = 'https://example-data.draftbit.com/books?_limit=20';
-
 export const BOOK_DETAILS_URL = 'https://example-data.draftbit.com/books/';
--- book-app/src/components/Navbar.jsx
+++ book-app/src/components/Navbar.jsx
@@ -7,11 +7,14 @@ const Navbar = () => {
     <div className='navbar'>
 
        <div>
+       <Link to="/"><h2>Home</h2></Link>
+        </div>
+       <div>
        <Link to="/"><h2>GreatReads</h2></Link>
         </div>
 
        <div>
-        <Link to="/favorites"><p>Favorites</p></Link>
+        <Link to="/favorites"><h2>Favorites</h2></Link>
       </div>
 
         </div>
